9331cba513733fe2d1d1fd4be1c43396f066097b
t3mp14r3 <vsuhovey@outlook.com>
2023-03-05 13:38:48 +0300

    Added tailwind, simple index and records page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="bg-slate-200 w-full h-screen flex justify-center select-none">
+    <div class="w-3/5 h-fit bg-gray-50 py-6 px-12 mt-16 rounded-2xl flex justify-between items-center shadow-lg">
+        <a href="/" class="text-4xl font-bold text-gray-700">Finz</a>
+        <div class="flex justify-end gap-8 text-lg text-gray-700">
+            <a href="/records" class="hover:text-gray-500 transition duration-200">Records</a>
+            <a href="/records" class="hover:text-gray-500 transition duration-200">Statistics</a>
+        </div>
+    </div>
+</main>
